725b8300d7611de993f1ae74cf7e88d55e579aca
David <dmartin5150@icloud.com>
2022-08-15 21:30:29 -0500

    added signin component, signInput component and button component

--- src/routes/sign-in/sign-in.components.jsx
+++ src/routes/sign-in/sign-in.components.jsx
@@ -1,20 +1,36 @@
-import {signInWithGooglePopup,createUserDocumentFromAuth} from '../../utils/firebase/firebase.utils';
+// import { useEffect } from "react";
+// import { getRedirectResult } from "firebase/auth";
 
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+  signInWithGoogleRedirect,
+} from "../../utils/firebase/firebase.utils";
 
+import SignUpForm from "../../components/sign-up form/sign-up-form.component";
 
 const SignIn = () => {
+  // useEffect(() => {
+  //   const callGetRedirect = async () => {
+  //     const response = await getRedirectResult(auth);
+  //     if(response) {
+  //       const userDocRef = await createUserDocumentFromAuth(response.user);
+  //     }
+  //   };
+  //   callGetRedirect();
+  // }, []);
 
   const logGoogleUser = async () => {
-    const {user} = await signInWithGooglePopup();
+    const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
-  }
+  };
 
   return (
     <div>
       <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}>
-        Sign In with Google Popup
-      </button>
-    </div>);
-}
-export default SignIn
+      <button onClick={logGoogleUser}>Sign In with Google Popup</button>
+      <SignUpForm />
+    </div>
+  );
+};
+export default SignIn;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -4,15 +4,10 @@ import {
   signInWithPopup,
   signInWithRedirect,
   GoogleAuthProvider,
+  createUserWithEmailAndPassword,
 } from "firebase/auth";
 
-import {
-  getFirestore,
-  doc,
-  getDoc,
-  setDoc
-
-} from 'firebase/firestore';
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -20,8 +15,7 @@ const firebaseConfig = {
   authDomain: "crwn-clothing-db-a7aec.firebaseapp.com",
   projectId: "crwn-clothing-db-a7aec",
   storageBucket: "crwn-clothing-db-a7aec.appspot.com",
-  messagingSenderId:
-   "666743720787",
+  messagingSenderId: "666743720787",
   appId: "1:666743720787:web:2e628ac06079cdf6b6efa6",
 };
 
@@ -30,41 +24,48 @@ const firebaseApp = initializeApp(firebaseConfig);
 
 const provider = new GoogleAuthProvider();
 provider.setCustomParameters({
-  prompt: "select_account"
+  prompt: "select_account",
 });
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, provider);
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
-  const userDocRef = doc(db, 'users', userAuth.uid);
+export const createUserDocumentFromAuth = async (userAuth,additionalInformation = {}) => {
+  if (!userAuth) {
+    return;
+  }
+  const userDocRef = doc(db, "users", userAuth.uid);
 
   console.log(userDocRef);
-  const userSnapshot =  await getDoc(userDocRef);
+  const userSnapshot = await getDoc(userDocRef);
   console.log(userSnapshot);
   console.log(userSnapshot.exists());
 
   if (!userSnapshot.exists()) {
-    const {displayName, email} = userAuth;
+    const { displayName, email } = userAuth;
     const createdAt = new Date();
 
-  try {
-    await setDoc(userDocRef, {
-      displayName,
-      email,
-      createdAt
-    })
-
-  }catch (error) {
-    console.log('error creating users', error.message)
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation
+      });
+    } catch (error) {
+      console.log("error creating users", error.message);
+    }
   }
+  return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email,password) => {
+  if (!email || !password) {
+    return;
+  }
+  return await createUserWithEmailAndPassword(auth,email,password);
 }
-return userDocRef;
-
-
-}
-
-
-
