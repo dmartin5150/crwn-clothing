f429cd2822ed6d642854eb86842ec52d5c716d68
David <dmartin5150@icloud.com>
2022-08-15 22:21:06 -0500

    created authentication component and sign in form

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,15 +1,14 @@
 // import { useEffect } from "react";
 // import { getRedirectResult } from "firebase/auth";
 
-import {
-  signInWithGooglePopup,
-  createUserDocumentFromAuth,
-  signInWithGoogleRedirect,
-} from "../../utils/firebase/firebase.utils";
 
 import SignUpForm from "../../components/sign-up form/sign-up-form.component";
+import SignInForm from "../../components/sign-in form/sign-in-form.component";
+import './authentication.styles.scss';
 
-const SignIn = () => {
+
+
+const Authentication = () => {
   // useEffect(() => {
   //   const callGetRedirect = async () => {
   //     const response = await getRedirectResult(auth);
@@ -20,17 +19,13 @@ const SignIn = () => {
   //   callGetRedirect();
   // }, []);
 
-  const logGoogleUser = async () => {
-    const { user } = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(user);
-  };
+
 
   return (
-    <div>
-      <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}>Sign In with Google Popup</button>
+    <div className='authentication-container'>
+      <SignInForm />
       <SignUpForm />
     </div>
   );
 };
-export default SignIn;
+export default Authentication;
